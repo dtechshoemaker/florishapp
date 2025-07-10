ceb7e151f64db7704237beb0fcebcfada1211eae
dtechshoemaker <peterkimbeghi@gmail.com>
2025-07-10 02:02:20 +0100

    Edited the allowed host

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn florishsavings.wsgi
+gunicorn florishsavings.wsgi
--- florishsavings/settings.py
+++ florishsavings/settings.py
@@ -19,10 +19,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://florishapp.onrender.com/']
 
 
 # Application definition
