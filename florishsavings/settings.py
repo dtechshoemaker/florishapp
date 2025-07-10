6673d3e0227ca125f3d2e9ddcf7d7dd6f7a74ecc
dtechshoemaker <peterkimbeghi@gmail.com>
2025-07-10 01:55:59 +0100

    Collected Static

--- florishsavings/settings.py
+++ florishsavings/settings.py
@@ -114,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # ✅ add this line
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
