c09144d0f87644d872da115e70df7e0097730072
dtechshoemaker <peterkimbeghi@gmail.com>
2025-07-13 22:51:34 +0100

    create a new app for saving activities

--- savingapp/admin.py
+++ savingapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from .models import Customer, Contribution
+from .models import Customer
 
 admin.site.register(Customer)
-admin.site.register(Contribution)
---
+++ savingplan/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from savingapp.models import Customer
+
+class SavingPlan(models.Model):
+    name = models.CharField(max_length=100)
+    amount_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    duration_in_days = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class CustomerSaving(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='savings')
+    saving_plan = models.ForeignKey(SavingPlan, on_delete=models.CASCADE, blank="True", null="none",)
+    is_active = models.BooleanField(default=True)
+    total_contributed = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.customer.full_name} - {self.saving_plan.name}"
+
+
+"""
+Contribution is the daily payment by the user which will be used for the tabke
+for the user
+"""
+class Contribution(models.Model):
+    customer_saving = models.ForeignKey(
+        CustomerSaving,
+        null=True,  # allow saving without this value (optional)
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='contributions'
+    )
+    payment_date = models.DateField(default=timezone.now)
+    amount_paid = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        if self.customer_saving and self.customer_saving.customer:
+            return f"{self.customer_saving.customer.full_name} - {self.payment_date}"
+        return f"Unknown Customer - {self.payment_date}"
